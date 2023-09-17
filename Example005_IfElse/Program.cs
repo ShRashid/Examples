de37fb5be6a6c55722edbe5df46e680b7ddf4566
Rashid <rashid_74@mail.ru>
2023-09-17 17:59:40 +0300

    Example005_IFELSE

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string UserName = Console.ReadLine();
+if(UserName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(UserName);
+}
