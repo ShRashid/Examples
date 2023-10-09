a1145c5d4cad2967bfec4fb9d2e5fc61025d248c
Rashid <rashid_74@mail.ru>
2023-10-09 16:24:30 +0300

    Довлены домашние задание к уроку 6

---
+++ Example014_Recurs/Program.cs
@@ -0,0 +1,42 @@
+﻿string[,] table = new string[2, 5];
+// table[0,0] table[0,1] table[0,2] table[0,4]
+// table[1,0] table[1,1] table[1,2] table[1,4]
+table[1,2] = "слово";
+// for (int rows = 0; rows < table.GetLength(0); rows++)
+// {
+//     for (int columns = 0; columns < table.GetLength(1); columns++)
+//     {
+//         Console.Write(table[rows, columns] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+int [,] matrix = new int[3, 4];
+
+void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write(matr[rows, columns] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+}
+//PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Example017_Home/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Console.WriteLine("Введите количество чисел для ввода: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+
+// for(int i = 0; i < M; i++)
+// {
+//     System.Console.WriteLine($"Ввидете {i+1}-е число: ");
+//     if(Convert.ToInt32(Console.ReadLine()) > 0) count++;
+// }
+
+// System.Console.WriteLine("Количество положительных чисел = "+count);
+
+//Задача 43: Напишите программу, которая найдёт точку 
+//пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+    Console.WriteLine("Введите коэффициенты b1 и k1 для первой прямой:");
+    double b1 = Convert.ToDouble(Console.ReadLine()),
+    k1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите коэффициенты b2 и k2 для второй прямой:");
+    double b2 = Convert.ToDouble(Console.ReadLine()),
+    k2 = Convert.ToDouble(Console.ReadLine());
+    double x = 0;
+    double y = 0;
+       
+    if (GetPoint())
+    {
+        Console.WriteLine($"Координаты точки пересечения: ({x}, {y})");
+    }
+    else
+    {
+        Console.WriteLine("Прямые не пересекаются.");
+    }
+
+bool GetPoint()
+{
+   
+    if (k1 == k2)
+    {
+         return false;
+    }     
+    else 
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k2 * x + b2;
+        return true;
+    }
+}
+
+
